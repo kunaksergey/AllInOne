a1e0a6f185660f525a0c3ea10ec1e3f23381fa5f
Sa <kunaksergey@ukr.net>
2017-03-04 14:50:05 +0200

    03.03.2017 13.21

---
+++ src/main/java/ua/shield/test/MainDriver.java
@@ -0,0 +1,16 @@
+package ua.shield.test;
+
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+/**
+ * Created by sa on 03.03.17.
+ */
+public class MainDriver {
+    public static void main(String[] args) {
+
+    }
+}
